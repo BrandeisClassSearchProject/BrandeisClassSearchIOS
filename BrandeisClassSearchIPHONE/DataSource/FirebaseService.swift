97e9bfd1afb62e7937ac08019c3855ad0a25d96c
Alex Hu <alexhubnds@icloud.com>
2017-04-02 12:05:59 -0400

    ipad

--- BrandeisClassSearchIPHONE/DataSource/FirebaseService.swift
+++ BrandeisClassSearchIPHONE/DataSource/FirebaseService.swift
@@ -9,10 +9,15 @@ import Firebase
 
 class FirebaseService {
     
-    init(){
-        //.....
+    static let shared = FirebaseService()
+    
+    var BASE_REF: FIRDatabaseReference {
+        return FIRDatabase.database().reference()
     }
     
+    var SEMESTER_REF: FIRDatabaseReference {
+        return BASE_REF.child("1171")
+    }
     
     //This function will search the lastest term in Firebase 
     //If the corresponding course is found with given ID
@@ -24,8 +29,25 @@ class FirebaseService {
     //The headers are crutial
     //All headers are listed in the Attribute enum in CourseDataItem.swift in CourseDataStructForMainList folder
     func search(courseID: String) -> [String]{
-        //...
-        return []
+        
+        var matchingCourses = [String]()
+        
+        BASE_REF.observeSingleEvent(of: .value, with: { (snapshot) in
+            for semester in snapshot.children.allObjects as! [FIRDataSnapshot] {
+                print("Semester: \(semester.key)")
+                for course in semester.children.allObjects as! [FIRDataSnapshot] {
+                    if(course.hasChild("NAME")) {
+                        let courseName: String = (course.childSnapshot(forPath: "NAME").value as! String).lowercased()
+                        if courseName.range(of: courseID.lowercased()) != nil || course.key.lowercased().range(of: courseID.lowercased()) != nil {
+                            matchingCourses.append(course.key)
+                        }
+                    }
+                    
+                }
+            }
+            
+        })
+        return matchingCourses
     }
     
     
@@ -33,13 +55,44 @@ class FirebaseService {
     //and a dictionary(map) of course id to course names
     //These will be called only once in CourseDictionary initialzation
     func nameToId() -> [String:String] {
-        //...
-        return ["":""]
+        
+        var nameIdDict = [String:String]()
+        
+        BASE_REF.observe(.value, with: { (snapshot) in
+            for semester in snapshot.children.allObjects as! [FIRDataSnapshot] {
+                for data in semester.children.allObjects as! [FIRDataSnapshot] {
+                    if data.hasChild("NAME") {
+                        let className = data.childSnapshot(forPath: "NAME").value as! String
+                        let classId = data.key
+                        nameIdDict[className] = classId
+                    }
+                }
+            }
+            
+        })
+        
+        return nameIdDict
     }
     
     func idToName() -> [String:String] {
-        //...
-        return ["":""]
+        
+        var idNameDict = [String:String]()
+        
+        BASE_REF.observe(.value, with: { (snapshot) in
+            for semester in snapshot.children.allObjects as! [FIRDataSnapshot] {
+                for data in semester.children.allObjects as! [FIRDataSnapshot] {
+                    if data.hasChild("NAME") {
+                        let className = data.childSnapshot(forPath: "NAME").value as! String
+                        let classId = data.key
+                        idNameDict[classId] = className
+                    }
+                }
+            }
+            
+        })
+        
+        return idNameDict
+
     }
     
     
