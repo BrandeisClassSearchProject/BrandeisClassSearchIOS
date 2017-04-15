dc44853ecb4a7b6f44344906e82a73d3d1aa6084
Alex Hu <alexhubnds@icloud.com>
2017-04-15 02:03:21 -0400

    more cells

---
+++ BrandeisClassSearchIPHONE/MainTableCells/TableCellLocation.swift
@@ -0,0 +1,24 @@
+//
+//  TableCellLocation.swift
+//  BrandeisClassSearchIPHONE
+//
+//  Created by Yuanze Hu on 4/15/17.
+//  Copyright © 2017 Yuanze Hu. All rights reserved.
+//
+
+import UIKit
+
+class TableCellLocation: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BrandeisClassSearchIPHONE/MainTableCells/TableCellReq.swift
@@ -0,0 +1,79 @@
+//
+//  TableCellReq.swift
+//  BrandeisClassSearchIPHONE
+//
+//  Created by Yuanze Hu on 4/15/17.
+//  Copyright © 2017 Yuanze Hu. All rights reserved.
+//
+
+import UIKit
+
+class TableCellReq: UITableViewCell {
+
+    @IBOutlet var place1: UIButton!
+    
+    @IBOutlet var place2: UIButton!
+    
+    @IBOutlet var place3: UIButton!
+    
+    @IBOutlet var place4: UIButton!
+    
+    @IBOutlet var place5: UIButton!
+    
+    func visualize(){
+        
+    }
+    
+
+}
+
+
+
+
+//<option value="CA"  title="CA School of Creative Arts Distribution Req">
+//ca
+//</option>
+//
+//<option value="FL"  title="Foreign Language Requirement">
+//fl
+//</option>
+//
+//<option value="HUM"  title="School of Humanities Distribution Requirement">
+//hum
+//</option>
+//
+//<option value="NW"  title="Non-Western and Comparative Studies Requirement">
+//nw
+//</option>
+//
+//<option value="OC"  title="Oral Communication">
+//oc
+//</option>
+//
+//<option value="PE-1"  title="Physical Education 1 Course">
+//pe-1
+//</option>
+//
+//<option value="QR"  title="QR Quantitative Reasoning Requirement">
+//qr
+//</option>
+//
+//<option value="QR1"  title="Quantitative Reasoning Lecture Course">
+//qr1
+//</option>
+//
+//<option value="SN"  title="SN School of Science Distribution Requirement">
+//sn
+//</option>
+//
+//<option value="SS"  title="SS School of Social Sci Distribution Requirement">
+//ss
+//</option>
+//
+//<option value="UWS"  title="UWS University Writing Seminar">
+//uws
+//</option>
+//
+//<option value="WI"  title="WI Writing Intensive">
+//wi
+
